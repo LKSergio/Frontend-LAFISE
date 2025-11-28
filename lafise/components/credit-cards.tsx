9aac48723523ee1c5909949487a3ce92bd910e0b
LKSergio <zackmelendez477@gmail.com>
2025-11-28 16:04:30 -0600

    Correción del tamaño en los componentes de las tarjetas

--- lafise/components/credit-cards.tsx
+++ lafise/components/credit-cards.tsx
@@ -49,11 +49,11 @@ export function CreditCards() {
   return (
     <section>
       <h2 className="text-xl font-semibold text-foreground mb-4 text-gray-800">Mis tarjetas</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
         {cards.map((card) => (
           <div
             key={card.id}
-            className={`${colorClasses[card.color]} relative overflow-hidden rounded-xl p-5 text-white min-h-[180px] flex flex-col justify-between shadow-lg`}
+            className={`${colorClasses[card.color]} relative overflow-hidden rounded-xl p-4 text-white min-h-[150px] flex flex-col justify-between shadow-lg`}
           >
             {/* Logo LAFISE */}
             <div className="flex justify-between items-start">
@@ -67,24 +67,24 @@ export function CreditCards() {
               src="/Lafise_Card.png"
               alt="Banco LAFISE background"
               width={260}
-              height={50}
-              className="absolute right-50 top-17 object-contain opacity-[0.10] pointer-events-none select-none translate-x-60 -translate-y-20"
+              height={40}
+              className="absolute right-50 top-17 object-contain opacity-[0.05] pointer-events-none select-none translate-x-60 -translate-y-20"
               aria-hidden="true"
               priority
             />
 
-            <div className="flex items-center gap-7 text-lg font-mono tracking-widest px-20 py-12">
+            <div className="flex items-center gap-6 text-lg font-mono tracking-widest px-16 py-4">
               <span>{card.firstDigits}</span>
               <span>****</span>
               <span>****</span>
               <span>{card.lastDigits}</span>
             </div>
 
-            <div className="flex items-end">
+            <div className="flex items-end mt-0">
               <div>
                 <p className="text-sm font-medium">{card.holderName}</p>
               </div>
-              <div className="text-center px-20 py-0">
+              <div className="text-center px-16 py-0">
                 <p className="text-[10px] opacity-70">Expire date</p>
                 <p className="text-sm font-medium">{card.expiryDate}</p>
               </div>
