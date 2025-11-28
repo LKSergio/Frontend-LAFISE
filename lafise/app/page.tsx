b2841d65cae3aedec4161b39cfcf577085a9a612
LKSergio <zackmelendez477@gmail.com>
2025-11-28 13:58:54 -0600

    Uso de la función MainContent para mostrar el contenido de la vista de forma mas dinamica

--- lafise/app/page.tsx
+++ lafise/app/page.tsx
@@ -1,9 +1,7 @@
 import { SidebarProvider } from "@/contexts/sidebar-context"
 import { Sidebar } from "@/components/sidebar"
 import { Header } from "@/components/header"
-import { CreditCards } from "@/components/credit-cards"
-import { Accounts } from "@/components/accounts"
-import { RecentTransactions } from "@/components/recent-transactions"
+import { MainContent } from "@/components/main-content"
 
 export default function Home() {
   return (
@@ -13,11 +11,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col">
           <Header />
           <main className="flex-1 p-6 overflow-auto">
-            <div className="space-y-8">
-              <CreditCards />
-              <Accounts />
-              <RecentTransactions />
-            </div>
+            <MainContent />
           </main>
         </div>
       </div>
