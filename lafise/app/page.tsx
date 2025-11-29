f8d8933eda79273378603af20574f5691be01582
LKSergio <zackmelendez477@gmail.com>
2025-11-28 20:19:52 -0600

    Archivo modificado para envolver con UserProvider para proveer contexto global

--- lafise/app/page.tsx
+++ lafise/app/page.tsx
@@ -1,20 +1,23 @@
 import { SidebarProvider } from "@/contexts/sidebar-context"
+import { UserProvider } from "@/contexts/user-context"
 import { Sidebar } from "@/components/sidebar"
 import { Header } from "@/components/header"
 import { MainContent } from "@/components/main-content"
 
 export default function Home() {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen bg-gray-50">
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          <Header />
-          <main className="flex-1 p-6 overflow-auto">
-            <MainContent />
-          </main>
+    <UserProvider>
+      <SidebarProvider>
+        <div className="flex min-h-screen bg-gray-50">
+          <Sidebar />
+          <div className="flex-1 flex flex-col">
+            <Header />
+            <main className="flex-1 p-6 overflow-auto">
+              <MainContent />
+            </main>
+          </div>
         </div>
-      </div>
-    </SidebarProvider>
+      </SidebarProvider>
+    </UserProvider>
   )
 }
